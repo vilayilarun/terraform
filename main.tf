935c0591d54310bc0ba37d7644174392087899d6
arun <vilayilarun@gmail.com>
2022-06-17 07:13:33 +0400

    added sg  configuration

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable vpc-cidr-block {}
 variable subnet-cidr-block {}
 variable environment {}
 variable avail-zone {}
+variable myip {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc-cidr-block
@@ -42,4 +43,41 @@ resource "aws_internet_gateway" "myapp-igw" {
     "Name" = "${environment}-igw"
   }
   
+}
+
+resource "aws_route_table_association" "a-rtb" {
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-rtb.id
+  
+}
+
+resource "aws_security_group" "myapp-sg" {
+  name = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.myip]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = [var.myip]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+  
+  tags = {
+    Name = "${var.environment}-sg"
+  }
 }
