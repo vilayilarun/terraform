12536b0214dd21d3f809ab74f5df0b15cfd994a7
arun <vilayilarun@gmail.com>
2022-06-23 21:06:29 +0400

    modules added

--- main.tf
+++ main.tf
@@ -10,39 +10,12 @@ resource "aws_vpc" "myapp-vpc" {
   
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.subent-cidr_block
-  availability_zone = var.avail-zone
-  tags = {
-    "Name" = "${environment}-subnet"
-  }
-}
-
-resource "aws_route_table" "myapp-rtb" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    "Name" = "${environment}-rtb"
-  }
-  
-}
-
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  tags_all = {
-    "Name" = "${environment}-igw"
-  }
-  
-}
-
-resource "aws_route_table_association" "a-rtb" {
-  subnet_id = aws_subnet.myapp-subnet-1.id
-  route_table_id = aws_route_table.myapp-rtb.id
-  
+module "myapp-subnet" {
+  source = "./modules/subnet"
+  myapp-vpc = aws_vpc.myapp-vpc.id
+  subnet-cidr-block = var.subnet-cidr-block
+  environment = var.environment
+  avail-zone = var.avail-zone
 }
 
 resource "aws_security_group" "myapp-sg" {
