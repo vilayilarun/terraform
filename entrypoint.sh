b13581314d07828770791340adbd1b6a3aade1b0
arun <vilayilarun@gmail.com>
2022-06-28 23:43:01 +0400

    EKS configuration added

--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo dnf update -y && sudo dnf install docker -y
-sudo usermode -aG docker $(whoami)
-docker run -d -p 8080:80 nginx
--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ec2-public-ip" {
-  value = module.webserver.public_ip
-  
-}
